c40167cbe0d859367a47dad83a3f37f53a50f3f1
Alex Marz <alexhmarz@gmail.com>
2020-10-13 23:58:11 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
     counter = 0 
     board.each do |xo| 
-      if xo = "X" || xo = "O"
+      if xo == "X" || xo == "O"
         counter += 1 
       end
     end
@@ -9,4 +9,4 @@ def turn_count(board)
 end 
 
 def current_player(board)
-  
+end
